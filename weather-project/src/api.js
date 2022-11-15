3154131f9c4e8ef74d2719bc0c0e1fc5ac1b295e
Prashant Singh <singh123singhprashant@gmail.com>
2022-11-15 15:16:12 +0530

    weather-app

---
+++ weather-project/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "065912858bc22db810ed9614b34f8801",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "065912858bc22db810ed9614b34f8801"; 
